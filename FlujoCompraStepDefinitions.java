52b3f314dccd775a265d9a38dec804e9aa449d6f
FernandaLara <ferchalpropio00@gmail.com>
2025-09-15 22:56:28 -0500

    Add files via upload

---
+++ FlujoCompraStepDefinitions.java
@@ -0,0 +1,25 @@
+PROYECTO: Pruebas de la API de Signup/Login de DemoBlaze con Karate DSL
+
+REQUISITOS:
+- Java 11+
+- Maven 3.6+
+- Git
+- IDE opcional: IntelliJ IDEA, VSCode, etc.
+
+INSTALACIÓN Y EJECUCIÓN:
+1. Clona el repositorio:
+   git clone https://github.com/tuusuario/karate-demoblaze-api-tests.git
+
+2. Entra a la carpeta del proyecto:
+   cd karate-demoblaze-api-tests
+
+3. Ejecuta las pruebas con Maven:
+   mvn test
+
+4. Ver los reportes:
+   Abre el archivo `target/karate-reports/karate-summary.html` en tu navegador.
+
+VERSIÓN DE TECNOLOGÍAS:
+- Karate DSL: 1.4.1 o superior
+- Java: 11 o superior
+- Maven: 3.6.3 o superior
